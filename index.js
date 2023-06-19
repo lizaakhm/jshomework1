b854dc20af94003ed89aaed605e8334db02de556
lizaakhm <131793303+lizaakhm@users.noreply.github.com>
2023-06-19 18:29:31 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+console.log("the square numbers from 1 to 10")
+
+for (let x = 0; x <= 5; x++) {
+    let z = x * x;
+    console.log(z);
+}
+
+console.log("sum of numbers from 1 to 10")
+
+let k = 0
+let num = 10
+
+for( let i=0; i <=num; i++){
+k += i
+}
+console.log(k)
+
+
+
+console.log("average from 1 to 10");
+
+let n = 0;
+let m = 10;
+
+for(let l=0; l<=m; l++){
+    n+=l
+}
+
+let o = n/ m
+
+console.log(o)
+
+
+
+console.log("reverse word and convert each character in a string to uppercase")
+
+let p = "Hello, Lizi "
+let q = ""
+
+for (let r = p.length;  r>0; r--){
+    q+=p[r-1]
+}
+console.log(q)
+
